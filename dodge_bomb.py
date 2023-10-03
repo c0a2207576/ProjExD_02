48b3648d7f48d9c4f7e4bae0af16f69eb2fcaf43
naruju <c0a2207576@edu.teu.ac.jp>
2023-10-03 17:21:19 +0900

    演習

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,87 +2,118 @@ import random
 import sys
 import pygame as pg
 
-
 WIDTH, HEIGHT = 1600, 900
 
-
-delta = {  # 練習３：移動量辞書
+delta = {
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
+    pg.K_UP and pg.K_LEFT:(-5,-5),
+    pg.K_UP and pg.K_RIGHT:(+5,-5),
+    pg.K_DOWN and pg.K_LEFT:(-5,+5),
+    pg.K_DOWN and pg.K_RIGHT:(+5,+5)
 }
 
-
 def check_bound(obj_rct: pg.Rect):
-    """
-    引数：こうかとんRectかばくだんRect
-    戻り値：タプル（横方向判定結果，縦方向判定結果）
-    画面内ならTrue，画面外ならFalse
-    """
     yoko, tate = True, True
-    if obj_rct.left < 0 or WIDTH < obj_rct.right: # 横方向判定
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
         yoko = False
-    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: # 縦方向判定
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
     return yoko, tate
-
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    """こうかとん"""
+
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
-    kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
-    """ばくだん"""
-    bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
-    bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
+    kk_rct.center = (900, 400)
+
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
-    bd_rct = bd_img.get_rect()  # 練習１：SurfaceからRectを抽出する
+    bd_rct = bd_img.get_rect()
     x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    bd_rct.center = (x, y)  # 練習１：Rectにランダムな座標を設定する
-    vx, vy = +5, +5  # 練習２：爆弾の速度
+    bd_rct.center = (x, y)
+    vx, vy = +5, +5
 
     clock = pg.time.Clock()
     tmr = 0
+
+    # 追加の変数と画像をロード
+    kk_direction = 0  # 初期向きは右
+
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
 
-        if kk_rct.colliderect(bd_rct):  # 練習５：ぶつかってたら
+        if kk_rct.colliderect(bd_rct):
             print("ゲームオーバー")
             return
 
         screen.blit(bg_img, [0, 0])
 
-        """こうかとん"""
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in delta.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
-                sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
-        kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
-        if check_bound(kk_rct) != (True, True):  # 練習４：はみだし判定
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) 
-        screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
-        """"ばくだん"""
-        bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+                if key == pg.K_RIGHT:
+                    kk_direction = 0
+                elif key == pg.K_UP:
+                    kk_direction = 1
+                elif key == pg.K_DOWN:
+                    kk_direction = 2
+                elif key == pg.K_LEFT:
+                    kk_direction = 3
+                elif key == (pg.K_UP and pg.K_RIGHT):
+                    kk_direction = 4
+                elif key == (pg.K_UP and pg.K_LEFT):
+                    kk_direction = 5
+                elif key == (pg.K_DOWN and pg.K_RIGHT):
+                    kk_direction = 6
+                elif key == (pg.K_DOWN and pg.K_LEFT):
+                    kk_direction = 7
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
+        # kk_directionに応じて画像を回転して表示
+        if kk_direction == 0:  # 右向き
+            rotated_kk_img = pg.transform.flip(kk_img, True, False)
+        elif kk_direction == 1:  # 上向き
+            rotated_kk_img = pg.transform.flip(pg.transform.rotate(kk_img, 90), False, True)
+        elif kk_direction == 2:  # 下向き
+            rotated_kk_img = pg.transform.flip(pg.transform.rotate(kk_img, 270), False, True)
+        elif kk_direction == 3:  # 左向き
+            rotated_kk_img = kk_img
+        elif kk_direction == 4:  # 右上斜め
+            rotated_kk_img = pg.transform.rotate(kk_img, -45)
+        elif kk_direction == 5:  # 左上斜め
+            rotated_kk_img = pg.transform.rotate(kk_img, 45)
+        elif kk_direction == 6:  # 右下斜め
+            rotated_kk_img = pg.transform.flip(pg.transform.rotate(kk_img, 45), False, True)
+        elif kk_direction == 7:  # 左下斜め
+            rotated_kk_img = pg.transform.flip(pg.transform.rotate(kk_img, -45), True, False)
+        screen.blit(rotated_kk_img, kk_rct)
+
+        bd_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bd_rct)
-        if not yoko:  # 練習４：横方向にはみ出たら
+        if not yoko:
             vx *= -1
-        if not tate:  # 練習４：縦方向にはみ出たら
+        if not tate:
             vy *= -1
-        screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
-        
+        screen.blit(bd_img, bd_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
@@ -91,4 +122,3 @@ if __name__ == "__main__":
     sys.exit()
 
 
-    
